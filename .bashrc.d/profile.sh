07095d0927897a9851050739310cadc9fa5536da
Nicholas Berlette <nick@berlette.com>
2021-12-22 11:30:47 -0800

    update profile.sh

--- .bashrc.d/profile.sh
+++ .bashrc.d/profile.sh
@@ -77,15 +77,18 @@ export GIT_PS1_SHOWCOLORHINTS=${GIT_PS1_SHOWCOLORHINTS:-1}
 export GIT_PS1_SHOWDIRTYSTATE=${GIT_PS1_SHOWDIRTYSTATE:-1}
 export GIT_PS1_SHOWSTASHSTATE=${GIT_PS1_SHOWSTASHSTATE:-1}
 export GIT_PS1_SHOWUNTRACKEDFILES=${GIT_PS1_SHOWUNTRACKEDFILES:-1}
-export GIT_PS1_SHOWUPSTREAM=${GIT_PS1_SHOWUPSTREAM:-'auto verbose name git'}
+export GIT_PS1_SHOWUPSTREAM=${GIT_PS1_SHOWUPSTREAM:-'auto'}
 export GIT_PS1_OMITSPARSESTATE=${GIT_PS1_OMITSPARSESTATE:-1}
 export GIT_PS1_STATESEPARATOR=${GIT_PS1_STATESEPARATOR:-' '}
 export GIT_PS1_DESCRIBE_STYLE=${GIT_PS1_DESCRIBE_STYLE:-tag}
 export GIT_PS1_HIDE_IF_PWD_IGNORED=${GIT_PS1_HIDE_IF_PWD_IGNORED:-''}
 
-export GIT_PS1_PREFIX=${GIT_PS1_PREFIX:-"\[\e]0;\u \W\e\]\[\e[1;7;33m\] \u \[\e[0;7;36m\] \w \[\e[0;1;7m\] "}
+export GIT_PS1_PREFIX=${GIT_PS1_PREFIX:-"\[\e]0;\u \W\e\]\[\e[1;7;33m\] \u \[\e[0;7;36m\] \w \[\e[0;1m\] "}
 export GIT_PS1_SUFFIX=${GIT_PS1_SUFFIX:-"\[\e[0m\]\n\[\e[1;32;6m\]\$\[\e[0m\] "}
-export GIT_PS1_FORMAT=${GIT_PS1_FORMAT:-" git:%s "}
+export GIT_PS1_FORMAT=${GIT_PS1_FORMAT:-" git:(%s) "}
+
+#### ADD yarn global bin to PATH; deduplicate
+PATH="$(yarn global bin):$PATH" && dedupe_path && export PATH;
 
 #### SAFELY ADD GLOBAL PKGS WITH YARN
 __global_add () {
@@ -100,9 +103,6 @@ __global_add degit typescript ts-standard ts-node
   && echo -e "defaultAgent=${defaultAgent:-pnpm}\nglobalAgent=${globalAgent:-yarn}\n" > "$HOME/.nirc";
 
 __global_add pnpm @antfu/ni
-
-#### ADD yarn global bin to PATH; deduplicate
-PATH="$(yarn global bin):$PATH" && dedupe_path && export PATH;
 
 #### PROMPT_COMMAND: ensure __git_ps1 exists -> export 
 if ! which __git_ps1 > /dev/null; then

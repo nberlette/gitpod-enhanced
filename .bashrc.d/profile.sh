2889387965d64aab2f36e4aaa953367143395b58
Nicholas Berlette <nick@berlette.com>
2021-12-22 11:22:29 -0800

    update profile.sh

--- .bashrc.d/profile.sh
+++ .bashrc.d/profile.sh
@@ -18,7 +18,6 @@ dedupe_path () {
     set_var "$pathvar_name" "$deduped_path"
 }
 
-
 #################################
 #### ALIASES
 #################################
@@ -59,7 +58,6 @@ alias mkdir='mkdir -p'
 alias ..="cd .."
 alias cd..='cd ..'
 alias t='touch'
-alias commit='git commit -S'
 
 # my two most common typos
 alias gitignore='git ignore'
@@ -68,40 +66,48 @@ alias gitingore='git ignore'
 alias ingore='git ignore'
 
 #################################
-#### PROFILE.SH 
+#### PROFILE.SH - DEFAULTS
 #################################
 
-export TZ='America/Los_Angeles'
+#### Localization
+export TZ=${TZ:-'America/Los_Angeles'}
 
-export GIT_PS1_SHOWUPSTREAM='auto verbose name git' 
-export GIT_PS1_SHOWCOLORHINTS=1
-export GIT_PS1_SHOWDIRTYSTATE=1
-export GIT_PS1_SHOWUNTRACKEDFILES=1
-export GIT_PS1_DESCRIBE_STYLE='tag'
-export GIT_PS1_OMITSPARSESTATE=1
-export GIT_PS1_STATESEPARATOR=''
-export GIT_PS1_SHOWSTASHSTATE=1
-export GIT_PS1_PREFIX="\[\e]0;\u \W\e\]\[\e[1;7;33m\] gitpod \[\e[0m\]\[\e[7;36m\] …/\W \[\e[0m\e[1m\] "
-export GIT_PS1_SUFFIX="\[\e[0m\e[1;32;6m\]\$\[\e[0m\] "
-export GIT_PS1_FORMAT=" git:%s "
+#### TODO: #1 write documentation on available options and how to customize
+export GIT_PS1_SHOWCOLORHINTS=${GIT_PS1_SHOWCOLORHINTS:-1}
+export GIT_PS1_SHOWDIRTYSTATE=${GIT_PS1_SHOWDIRTYSTATE:-1}
+export GIT_PS1_SHOWSTASHSTATE=${GIT_PS1_SHOWSTASHSTATE:-1}
+export GIT_PS1_SHOWUNTRACKEDFILES=${GIT_PS1_SHOWUNTRACKEDFILES:-1}
+export GIT_PS1_SHOWUPSTREAM=${GIT_PS1_SHOWUPSTREAM:-'auto verbose name git'}
+export GIT_PS1_OMITSPARSESTATE=${GIT_PS1_OMITSPARSESTATE:-1}
+export GIT_PS1_STATESEPARATOR=${GIT_PS1_STATESEPARATOR:-' '}
+export GIT_PS1_DESCRIBE_STYLE=${GIT_PS1_DESCRIBE_STYLE:-tag}
+export GIT_PS1_HIDE_IF_PWD_IGNORED=${GIT_PS1_HIDE_IF_PWD_IGNORED:-''}
 
-which __git_ps1 > /dev/null || source "$HOME/.bashrc.d/10-prompt";
-
-export PROMPT_COMMAND="__git_ps1 \"$GIT_PS1_PREFIX\" \"$GIT_PS1_SUFFIX\" \"$GIT_PS1_FORMAT\"";
-
-#### CONFIGURE  @antfu/ni  ####
-if [ ! -f "$HOME/.nirc" ]; then 
-    echo -e "defaultAgent=${defaultAgent:-pnpm}\nglobalAgent=${globalAgent:-yarn}\n" > "$HOME/.nirc";
-fi
-
-#### ADD YARN GLOBAL BIN TO PATH ####
-set_var PATH "$(yarn global bin):$PATH"
-dedupe_path PATH
+export GIT_PS1_PREFIX=${GIT_PS1_PREFIX:-"\[\e]0;\u \W\e\]\[\e[1;7;33m\] \u \[\e[0;7;36m\] \w \[\e[0;1;7m\] "}
+export GIT_PS1_SUFFIX=${GIT_PS1_SUFFIX:-"\[\e[0m\]\n\[\e[1;32;6m\]\$\[\e[0m\] "}
+export GIT_PS1_FORMAT=${GIT_PS1_FORMAT:-" git:%s "}
 
 #### SAFELY ADD GLOBAL PKGS WITH YARN
-############################################
-__yarn_global_add () {
+__global_add () {
   for pkg in "${@}"; do which "$pkg" > /dev/null || yarn global add "$pkg"; done
 }
-#__yarn_global_add pnpm @antfu/ni degit typescript ts-standard ts-node @types/node
 
+#### GLOBAL: add degit + typescript support
+__global_add degit typescript ts-standard ts-node
+
+#### CONFIGURE  @antfu/ni 
+[ ! -f "$HOME/.nirc" ] \
+  && echo -e "defaultAgent=${defaultAgent:-pnpm}\nglobalAgent=${globalAgent:-yarn}\n" > "$HOME/.nirc";
+
+__global_add pnpm @antfu/ni
+
+#### ADD yarn global bin to PATH; deduplicate
+PATH="$(yarn global bin):$PATH" && dedupe_path && export PATH;
+
+#### PROMPT_COMMAND: ensure __git_ps1 exists -> export 
+if ! which __git_ps1 > /dev/null; then
+    [ -e /home/gitpod/.bashrc.d/*-prompt ] \
+      && source /home/gitpod/.bashrc.d/*-prompt;
+fi
+
+export PROMPT_COMMAND="__git_ps1 \"$GIT_PS1_PREFIX\" \"$GIT_PS1_SUFFIX\" \"$GIT_PS1_FORMAT\"";
